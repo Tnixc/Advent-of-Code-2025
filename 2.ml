d832484eeba50adfe8a1dddcff3e151e3a598c01
Tnixc <tnixxc@gmail.com>
2025-12-02 09:29:30 -0500

    day2: proper fp

--- 2.ml
+++ 2.ml
@@ -2,8 +2,6 @@ let input = String.trim {|
 #include "inputs/2"
 |} in
 
-let sum = ref 0 in
-
 let t =
   {|11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124|}
 in
@@ -13,7 +11,6 @@ let check1 str len =
   && String.sub str 0 (len / 2) = String.sub str (len / 2) (len / 2)
 in
 
-(* returns true if invalid *)
 let rec check2 str len span span_len =
   if len = 0 then true
   else if len < span_len then false
@@ -31,21 +28,20 @@ let s =
       | [ a; b ] -> (int_of_string a, int_of_string b)
       | _ -> assert false)
   |> List.map (fun (a, b) ->
-      let count = ref 0 in
-      for i = a to b do
-        let str = string_of_int i in
-        let len = String.length str in
-        let rec loop k =
-          if k > len / 2 then ()
-          else if check2 str len (String.sub str 0 k) k then (
-            count := !count + i;
-            ())
-          else loop (k + 1)
-        in
-        loop 1
-      done;
+      let rec range i acc =
+        if i > b then acc
+        else
+          let str = string_of_int i in
+          let len = String.length str in
+          let rec loop k count =
+            if k > len / 2 then count
+            else if check2 str len (String.sub str 0 k) k then count + i
+            else loop (k + 1) count
+          in
+          range (i + 1) (acc + loop 1 0)
+      in
 
-      !count)
+      range a 0)
   |> List.fold_left (fun acc x -> acc + x) 0
 in
 
