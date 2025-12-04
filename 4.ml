f2dd4814f1082c08629e0c24363f4bfbe88c3458
Tnixc <tnixxc@gmail.com>
2025-12-04 00:32:40 -0500

    day 4 p 2

--- 4.ml
+++ 4.ml
@@ -26,35 +26,56 @@ let parse_input str =
   Array.of_list ((border :: rows) @ [ border ])
 in
 
-let grid = parse_input input in
-let w = Array.length grid - 2 in
-let h = Array.length grid.(0) - 2 in
+let grid = ref (parse_input input) in
+let changed = ref true in
+let w = Array.length !grid - 2 in
+let h = Array.length !grid.(0) - 2 in
 
 let count = ref 0 in
-for y = 1 to h do
-  for x = 1 to w do
-    if grid.(y).(x) = '@' then
-      (* Printf.printf "%c" grid.(y).(x) *)
-      let neighbors =
-        [
-          (x - 1, y - 1);
-          (x, y - 1);
-          (x + 1, y - 1);
-          (x - 1, y);
-          (x + 1, y);
-          (x - 1, y + 1);
-          (x, y + 1);
-          (x + 1, y + 1);
-        ]
-      in
-      if
-        List.length
-          (List.filter
-             (fun l -> l)
-             (List.map (fun (i, j) -> grid.(j).(i) = '@') neighbors))
-        < 4
-      then count := !count + 1
-  done
+while !changed do
+  changed := false;
+
+  for y = 1 to h do
+    for x = 1 to w do
+      if !grid.(y).(x) = '@' then
+        let neighbors =
+          [
+            (x - 1, y - 1);
+            (x, y - 1);
+            (x + 1, y - 1);
+            (x - 1, y);
+            (x + 1, y);
+            (x - 1, y + 1);
+            (x, y + 1);
+            (x + 1, y + 1);
+          ]
+        in
+        if
+          List.length
+            (List.filter
+               (fun l -> l)
+               (List.map
+                  (fun (i, j) -> !grid.(j).(i) = '@' || !grid.(j).(i) = 'X')
+                  neighbors))
+          < 4
+        then (
+          count := !count + 1;
+          changed := true;
+          !grid.(y).(x) <- 'X')
+    done
+  done;
+
+  grid :=
+    Array.map
+      (fun row -> Array.map (fun cell -> if cell = 'X' then '.' else cell) row)
+      !grid
 done;
 
-Printf.printf "%d\n" !count
+Printf.printf "%d\n" !count;
+
+for x = 1 to w do
+  for y = 1 to h do
+    Printf.printf "%c" !grid.(y).(x)
+  done;
+  Printf.printf "\n"
+done
