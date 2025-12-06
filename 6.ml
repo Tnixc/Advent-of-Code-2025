c5b5182c594822861f666f49f434bfaf15251843
Tnixc <tnixxc@gmail.com>
2025-12-06 02:08:12 -0500

    cleanups

--- 6.ml
+++ 6.ml
@@ -8,7 +8,7 @@ let t =
   6 |98 |215|314
 *   |+  |*  |+  |}
 in
-(* Printf.printf "Input:\n%s\n" t; *)
+
 let split_last lst =
   match List.rev lst with
   | [] -> assert false
@@ -19,26 +19,29 @@ let nums, ops =
   String.split_on_char '\n' input
   |> List.map (fun l -> String.split_on_char '|' l |> Array.of_list)
   |> split_last
+  |> fun (a, b) -> (a, Array.map String.trim b)
 in
-let ops = Array.map String.trim ops in
 
-(* let i = ref 0 in *)
-(* let sum = *)
-(*   let nums = List.map (fun l -> Array.map int_of_string l) nums in *)
-(*   Array.map *)
-(*     (fun op -> *)
-(*       let operands = List.map (fun l -> l.(!i)) nums in *)
-(*       incr i; *)
-(*       match op with *)
-(*       | "*" -> List.fold_left (fun acc x -> acc * x) 1 operands *)
-(*       | "+" -> List.fold_left (fun acc x -> acc + x) 0 operands *)
-(*       | _ -> assert false) *)
-(*     ops *)
-(*   |> Array.fold_left ( + ) 0 *)
-(* in *)
+let p1 =
+  let i = ref 0 in
+  let nums =
+    List.map
+      (fun l -> Array.map (fun s -> int_of_string (String.trim s)) l)
+      nums
+  in
+  Array.map
+    (fun op ->
+      let operands = List.map (fun l -> l.(!i)) nums in
+      incr i;
+      match op with
+      | "*" -> List.fold_left (fun acc x -> acc * x) 1 operands
+      | "+" -> List.fold_left (fun acc x -> acc + x) 0 operands
+      | _ -> assert false)
+    ops
+  |> Array.fold_left ( + ) 0
+in
 
-(* Printf.printf "%d\n" sum; *)
-let sum =
+let p2 =
   let i = ref 0 in
   Array.map
     (fun op ->
@@ -51,40 +54,29 @@ let sum =
       in
       incr i;
 
-      List.iter
-        (fun x -> Printf.printf "[%s]\n" (String.concat "" x))
-        operands_h;
-
       let highest_len =
         List.fold_left (fun acc l -> max acc (List.length l)) 0 operands_h
       in
 
-      let base = List.init highest_len (fun i -> i) in
-
       let operands =
         List.map
-          (fun j ->
+          (fun idx ->
             List.fold_left
-              (fun acc y ->
-                acc ^ (List.nth_opt y j |> Option.value ~default:""))
+              (fun acc digits ->
+                acc ^ (List.nth_opt digits idx |> Option.value ~default:""))
               "" operands_h)
-          base
+          (List.init highest_len (fun i -> i))
         |> List.map (fun l -> String.trim l)
         |> List.filter (fun s -> s <> "")
         |> List.map int_of_string
       in
 
-      List.iter (fun x -> Printf.printf "%d " x) operands;
-      Printf.printf "\n";
-
       match op with
       | "*" -> List.fold_left (fun acc x -> acc * x) 1 operands
       | "+" -> List.fold_left (fun acc x -> acc + x) 0 operands
-      | "*" -> 0
-      | "+" -> 0
       | _ -> assert false)
     ops
   |> Array.fold_left ( + ) 0
 in
 
-Printf.printf "%d\n" sum
+Printf.printf "%d, %d\n" p1 p2
